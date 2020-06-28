a6fe4dd26b176b9c6c9e899f1e4dfa557496e6cf
Danilo Peña <nicolasdpf@gmail.com>
2020-06-28 07:50:45 -0500

    Creating tasks and saving into DB

--- to-do/to-do.js
+++ to-do/to-do.js
@@ -1,14 +1,33 @@
 const fs = require('fs');
 let todolist = [];
 
+const saveOnDB = () => {
+  let data = JSON.stringify(todolist);
+
+  fs.writeFile('db/data.json', data, (err) => {
+    err
+      ? console.error('Failed writing data file')
+      : console.log('success writing data file');
+  });
+};
+
+const loadDB = () => {
+  try {
+    todolist = require('../db/data.json');
+  } catch (error) {
+    todolist = [];
+  }
+};
+
 const create = (description) => {
+  loadDB();
   let toDo = {
     description,
     completed: false,
   };
 
   todolist.push(toDo);
-
+  saveOnDB();
   return toDo;
 };
 
